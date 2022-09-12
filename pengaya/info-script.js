4280f68344e7044b12c4da0567485c506835bd98
Bisnisokteto <110271297+Bisnisokteto@users.noreply.github.com>
2022-09-12 17:36:04 +0700

    yy
    Yy

--- pengaya/info-script.js
+++ pengaya/info-script.js
@@ -5,7 +5,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `*https://youtube.com/channel/UC3HcOB2U-fgsR8onRGXFM3w*
 
 Pengen mentahan nya ?
-Jual di admin, murah + no enc
+Nih ada santai bro....
+https://github.com/ImYanXiao/Elaina-MultiDevice
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
@@ -14,10 +15,10 @@ wa.me/6288223674569`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Cigegerotak",
+    mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'Chenn-BOT-MD-V1 MultiDevice',
+    title: 'Chenn-MD-BOT-V1 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
